6a776890fcd812c89d1844e04e0e2fc7826091b6
Fabien <napo-1er@hotmail.fr>
2025-12-04 22:08:08 +0100

    Initial commit: Vite + Firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBqlYXify9CDjqdYuCLBuAd7W76_Xd43Yg",
+    authDomain: "gestion-yoga.firebaseapp.com",
+    projectId: "gestion-yoga",
+    storageBucket: "gestion-yoga.firebasestorage.app",
+    messagingSenderId: "258116537790",
+    appId: "1:258116537790:web:f15de802569b5a4f22fdef"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
